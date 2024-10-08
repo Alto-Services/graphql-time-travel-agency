6a7c9660ed4f6f810ac90c524ade1eec3236ee56
Peter Daily <peter.daily@zoopla.co.uk>
2024-10-08 20:12:30 +0100

    booking mutation

--- src/basics/modules/booking/resolvers.ts
+++ src/basics/modules/booking/resolvers.ts
@@ -1,6 +1,30 @@
 import { Context } from '../../context';
+import { getTravelerById } from '../traveler/model';
 import { Booking } from './model';
+import { getTimePeriodById } from '../timePeriod/model';
 
 export type BookingResolverParent = { id: string } & Partial<Booking>;
 
-export const bookingResolvers = () => ({});
+export const bookingResolvers = () => ({
+  traveler: (parent: BookingResolverParent, args: unknown, { getBookingById }: Context) => {
+    const travelerId = parent.traveler ?? getBookingById(parent.id)?.traveler;
+
+    // this could be null but traveller is required
+    if (!travelerId) {
+      return null;
+    }
+
+    // this could be undefined but traveller is required
+    return getTravelerById(travelerId);
+  },
+
+  timePeriod: (parent: BookingResolverParent, args: unknown, { getBookingById }: Context) => {
+    const timePeriodId = parent.timePeriod ?? getTimePeriodById(parent.id)?.timePeriod;
+
+    return getTimePeriodById(timePeriodId);
+  },
+
+  status: (parent: BookingResolverParent, args: unknown, { getBookingById }: Context) => {
+    return parent.status ?? getBookingById(parent.id)?.status;
+  },
+});
