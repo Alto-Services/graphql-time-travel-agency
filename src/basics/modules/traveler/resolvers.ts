14fff22d1158a058b844e0727d30c10a85ddf429
Peter Daily <peter.daily@zoopla.co.uk>
2024-10-14 17:37:44 +0100

    questions

--- src/basics/modules/traveler/resolvers.ts
+++ src/basics/modules/traveler/resolvers.ts
@@ -6,9 +6,11 @@ import { getBookingById } from '../booking/model';
 type TravelerResolverParent = { id: string } & Partial<Traveler>; // why is this a partial type, as top level query will get the whole object
 
 export const travelerResolvers = () => ({
+    // do i need this method as the top level query returns same type
     name: (parent: TravelerResolverParent, args: unknown, { getTravelerById }: Context) => {
         return parent.name ?? getTravelerById(parent.id)?.name;
     },
+        // do i need this method as the top level query returns same type
     eraOfOrigin: (parent: TravelerResolverParent, args: unknown, { getTravelerById }: Context) => {
         return parent.eraOfOrigin ?? getTravelerById(parent.id)?.eraOfOrigin;
     },
